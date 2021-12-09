3fcae20753e22f3c22e4a81b0e3b90888588ef0d
An <annogo123@gmail.com>
2021-12-09 16:31:14 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 //Không cần /index.js hoặc có thể / đến mà ko cần .js
-import Baitap1 from "./baitap1";
-import Baitap2 from "./baitap2";
-import BTVN_REACTJS from "./BTVN_B1_react";
+import UserManagement from "./user-management";
 
 function App() {
   return <div>
-    <BTVN_REACTJS/>
+    <UserManagement/>
   </div>;
 }
 
